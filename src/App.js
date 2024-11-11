eb82664578f12d37a3265b3e2f880b1e9e0bdc94
Akshay <akshay.deshmukh0543@gmail.com>
2024-11-12 00:10:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import DocumentUpload from "./components/DocumentUpload";
+
+function App() {
+  return (
+    <div className="App min-h-screen flex items-center justify-center bg-gray-100">
+      <DocumentUpload />
+    </div>
+  );
+}
+
+export default App;
